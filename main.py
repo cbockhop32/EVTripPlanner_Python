3283e47c4ab2e6927d3ec1b7551791e05c3a9be9
cbockhop32 <66850542+cbockhop32@users.noreply.github.com>
2024-03-08 12:30:39 -0800

    more code clean up

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import pandas as pd
 import network
 import sys
 
-car_range = 198
 
-# Get charger list from CSV file and add labels within pandas df
-col_names = ['City','Latitude','Longitude']
-df = pd.read_csv('network.csv', names= col_names, header=None)
+def main(*args):
 
-starting_charger, ending_charger = sys.argv[1], sys.argv[2]
 
-# Initialize new network and then build network of chargers
-new_network = network.Network()
-new_network.build_network(df,col_names)
-new_network.add_edges_to_network(car_range)
+    car_range = 198
 
-new_network.calculate_shortest_path(starting_charger, ending_charger)
+    # Get charger list from CSV file and add labels within pandas df
+    col_names = ['City','Latitude','Longitude']
+    df = pd.read_csv('network.csv', names= col_names, header=None)
+
+    starting_charger, ending_charger = args[0], args[1]
+
+    # Initialize new network and then build network of chargers
+    new_network = network.Network()
+    new_network.build_network(df,col_names)
+    new_network.add_edges_to_network(car_range)
+
+    new_network.calculate_shortest_path(starting_charger, ending_charger)
+
+
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
 
 
